be921be10762d306fa391bf7791a0962382c9416
zhouhongmin <1131096209@qq.com>
2024-02-19 16:15:29 +0800

    update

--- mission/mission_pybind.cpp
+++ mission/mission_pybind.cpp
@@ -13,6 +13,7 @@ PYBIND11_MODULE(_C, m) {
   m.doc() = "mission.cpp python binding";
 
   py::class_<MissionConfig>(m, "MissionConfig")
+    .def(py::init())
     // .def_readonly("dtype", &MissionConfig::dtype)
     .def_readwrite("vocab_size", &MissionConfig::vocab_size)
     .def_readwrite("hidden_size", &MissionConfig::hidden_size)
@@ -43,11 +44,39 @@ PYBIND11_MODULE(_C, m) {
     .def_property_readonly("tokenizer", [](const Pipeline &self){ return self.tokenizer.get(); })
     .def("write_logfile", &Pipeline::write_logfile)
     .def("generator", &Pipeline::generator)
-    .def("tokenize", &Pipeline::tokenize);
+    .def("tokenize", &Pipeline::tokenize)
+    .def("get_params", &Pipeline::get_params);
 
-  py::class_<gpt_params>(m, "gpt_params");
+  py::class_<gpt_params>(m, "gpt_params")
+    .def(py::init())
+    .def_readwrite("n_threads", &gpt_params::n_threads)
+    .def_readwrite("n_ctx", &gpt_params::n_ctx)
+    .def_readwrite("n_predict", &gpt_params::n_predict)
+    .def_readwrite("n_ctx", &gpt_params::n_ctx)
+    .def_readwrite("n_batch", &gpt_params::n_batch)
+    .def_readwrite("n_keep", &gpt_params::n_keep)
+    .def_readwrite("n_parallel", &gpt_params::n_parallel)
+    .def_readwrite("n_gpu_layers", &gpt_params::n_gpu_layers)
+    .def_readwrite("main_gpu", &gpt_params::main_gpu)
+    .def_readwrite("model", &gpt_params::model)
+    .def_readwrite("tiktoken_config", &gpt_params::tiktoken_config)
+    .def_readwrite("tiktoken_path", &gpt_params::tiktoken_path)
+    .def_readwrite("prompt", &gpt_params::prompt)
+    .def_readwrite("chatml", &gpt_params::chatml)
+    .def_readwrite("interactive", &gpt_params::interactive)
+    .def_readwrite("no_streaming", &gpt_params::no_streaming)
+    .def_readwrite("sparams", &gpt_params::sparams);
+
+  py::class_<llama_sampling_params>(m, "llama_sampling_params")
+    .def(py::init())
+    .def_readwrite("n_prev", &llama_sampling_params::n_prev)
+    .def_readwrite("top_k", &llama_sampling_params::top_k)
+    .def_readwrite("top_p", &llama_sampling_params::top_p)
+    .def_readwrite("temp", &llama_sampling_params::temp)
+    .def_readwrite("penalty_repeat", &llama_sampling_params::penalty_repeat);
+
   m.def("build_tokenizer", &build_tokenizer);
-  m.def("mission_params_parse", &mission_params_parse);
+//  m.def("mission_params_parse", &mission_params_parse);
 
 }
 
--- mission_cpp/__init__.py
+++ mission_cpp/__init__.py
@@ -11,6 +11,63 @@ def test_tokenizer(
 ):
     return _C.build_tokenizer(config_path, tiktoken_path)
 
+
 class Pipeline(_C.Pipeline):
-    def __init__(self):
-        pass
+    def __init__(self, n_threads: int, n_ctx: int = 512, n_predict: int = -1, n_batch: int = 512, n_keep: int = 0,
+                 n_parallel: int = 1, n_gpu_layers: int = -1, main_gpu: int = 0, model: str = './model/model.gguf',
+                 tiktoken_config: str = './model/tiktoken_config.json', tiktoken_path: str = './model/qwen.tiktoken',
+                 prompt: str = 'You are a helpful assistant.', chatml: bool = True, interactive: bool = True,
+                 no_streaming: bool = False, n_prev: int = 64, top_k: int = 40, top_p: int = 0.95, temp: int = 0.8,
+                 penalty_repeat: int = 1.1):
+        """
+        :param n_threads: number of threads
+        :param n_ctx: context size
+        :param n_predict: new tokens to predict
+        :param n_batch: batch size for prompt processing (must be >= 32 to use BLAS)
+        :param n_keep: number of tokens to keep from initial prompt
+        :param n_parallel: number of parallel sequences to decode
+        :param n_gpu_layers: number of layers to store in VRAM (-1 - use defaule)
+        :param main_gpu: the GPU that is used for scratch and small tensors
+        :param model: model path
+        :param tiktoken_config: config file for tiktoken (*.json)
+        :param tiktoken_path: tiktoken path (*.tiktoken)
+        :param prompt: initial prompt
+        :param chatml: chatml mode (used for models trained on chatml syntax)
+        :param interactive: interactive mode
+        :param no_streaming: chat without streaming results
+        :param n_prev: number of previous tokens to remember
+        :param top_k: <= 0 to use vocab size
+        :param top_p: 1.0 = disabled
+        :param temp: <= 0.0 to sample greedily, 0.0 to not output probabilities
+        :param penalty_repeat: 1.0 = disabled
+        """
+        sparams = _C.llama_sampling_params()
+        sparams.n_prev = n_prev
+        sparams.top_k = top_k
+        sparams.top_p = top_p
+        sparams.temp = temp
+        sparams.penalty_repeat = penalty_repeat
+
+        params = _C.gpt_params()
+        params.n_threads = n_threads
+        params.n_ctx = n_ctx
+        params.n_predict = n_predict
+        params.n_batch = n_batch
+        params.n_keep = n_keep
+        params.n_parallel = n_parallel
+        params.n_gpu_layers = n_gpu_layers
+        params.main_gpu = main_gpu
+        params.model = model
+        params.tiktoken_config = tiktoken_config
+        params.tiktoken_path = tiktoken_path
+        params.prompt = prompt
+        params.chatml = chatml
+        params.interactive = interactive
+        params.no_streaming = no_streaming
+        params.sparams = sparams
+
+        super().__init__(params)
+
+    def gen(self, prompt:str = '') -> str:
+        return self.generator(False, prompt)
+
